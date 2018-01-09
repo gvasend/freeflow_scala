d7a6a85757528156c53f1016355850823b1767e1
Jerry <gvasend@ubuntu01.simbolika.com>
2018-01-09 01:59:34 +0000

    tbd

--- scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
+++ scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
@@ -1,3 +1,5 @@
+// 
+
 package com.simbolika.fabric
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
@@ -76,5 +78,53 @@ val cancellable =
     case "init" =>
       println("init")
       println(name)
+    case "start" =>
+      println("start")
+//    Wait for a specific time
+//    Send start to downstream tasks
+
+    
   }
 }
+
+
+var id:Int = 0
+val imm_fruit_count = Map("apples" -> 4, "oranges" -> 5, "bananas" -> 6)
+println(imm_fruit_count("apples"))
+println(imm_fruit_count.contains("apples"))
+println(imm_fruit_count.getOrElse("melons", "peaches"))
+val mut_fruit_count = scala.collection.mutable.Map[String, Int]()
+
+mut_fruit_count("apples") = 4
+mut_fruit_count += ("oranges" -> 5, "bananas" -> 6)
+
+println(mut_fruit_count)
+
+mut_fruit_count -= "apples"
+
+mut_fruit_count.keySet
+
+mut_fruit_count.values
+
+// This is a nice feature of Scala Maps:
+val defaultMap = Map("foo" -> 1, "bar" -> 2).withDefaultValue(3)
+defaultMap("qux")
+
+
+val task_status = scala.collection.mutable.Map[String, Int]()
+
+for each task in task_list:
+    task_status[task] = 0
+	
+wait for random time
+	
+rcv msg == "wake":
+    task_status[sender.name] += 1
+    upstream = getUpStream(name)
+    for each task in upstream_list:
+        if task_status(task) == 0:
+            return
+    for each task in downstream:
+        task ! "wake"
+	
+
