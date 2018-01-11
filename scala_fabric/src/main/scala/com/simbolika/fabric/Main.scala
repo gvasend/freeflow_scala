363dc80f155e8e883043022fa07979812b597c65
Jerry <gvasend@ubuntu01.simbolika.com>
2018-01-11 12:03:27 +0000

    revised taskgraph

--- scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
+++ scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
@@ -9,6 +9,68 @@ import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import scala.concurrent.duration._
 
 
+class StaticTaskGraph(tasks: Map[String, Map[String, Any]]) {
+  val cache = tasks
+  var self_id: String = "null"
+  val status = collection.mutable.Map[String, String]()
+  status(self_id) = "none"
+
+  def set_self(name: String) { 
+    self_id = name 
+    status(self_id) = "incomplete"
+  }
+  def task_complete(name: String) { status(name) = "complete"}
+  def task_status(name: String) = { status(name) }
+  def task_pred(name: String) = { cache(name)("pred") }
+  def task_succ(name: String) = { cache(name)("succ") }
+  def task_details(name: String) = { cache(name) }
+  def get_tasks() = { cache.keys }
+  
+  def complete() { 
+    println("set task complete")
+    status(self_id) = "complete"
+  }
+  def state() = { 
+    println(s"id = $self_id")
+    status(self_id) }
+  def pred() = { 
+    cache(self_id)("pred") 
+  }
+  def start() = {
+    task_complete(self_id)
+    if (ready()) {
+          println(s"$self_id is running")
+          var lst = succ().asInstanceOf[List[String]]
+          lst.foreach(x => 
+          { 
+            if (x != "null") {
+              println(s"send to $x")
+            }
+          })
+    }
+  }
+  def succ() = { cache(self_id)("succ") }
+  def details() = { cache(self_id) }
+  def ready() = { 
+    var ready_state = true
+    var lst = pred().asInstanceOf[List[String]]
+    lst.foreach(x => 
+    { 
+      var stat = "incomplete"
+      if (status.contains(x)) {
+        stat = this.task_status(x)
+      }
+      if (x == "null") {
+        stat = "complete"
+      }
+//      println(s"stat = $stat")
+      ready_state &= (stat == "complete") } )
+//    println("final read ", ready_state)
+    ready_state
+  } 
+}
+
+
 object Main extends App {
 
   val system = ActorSystem("sentient_fabric")
@@ -21,10 +83,15 @@ class SFM() extends Actor {
 
   import context._
 
-  val tasks = List("a","b","c")
-  val this_job = Map("graph"->Map("process"->"graphit"),"section"->Map("process"->"section_doc"),"load"->Map("process"->"xml_load"))
-  val job1: ActorRef = system.actorOf(Props(new Job("job1a",this_job)), "job1")
-  val job2: ActorRef = system.actorOf(Props(new Job("job2a",this_job)), "job2")
+
+val map1 = Map("step1"->Map("process"->"/home/gvasend/sk_step1","succ"->List("step2","step3"),"pred"->List("null")),
+               "step2"->Map("process"->"sk_step2","pred"->List("step1"),"succ"->List("step3")),
+               "step3"->Map("process"->"sk_step3","pred"->List("step2","step1"),"succ"->List("null")))
+println(s"map1 = $map1")
+
+  val tg = new StaticTaskGraph(map1)
+  val job1: ActorRef = system.actorOf(Props(new Job("job1a",tg)), "job1")
+  val job2: ActorRef = system.actorOf(Props(new Job("job2a",tg)), "job2")
   
   
 //This will schedule to send the Tick-message
@@ -42,13 +109,13 @@ val cancellable =
 }
 
 
-class Job(name: String, tasks: Map[String, Map[String,String]]) extends Actor {
+class Job(name: String, tasks: StaticTaskGraph) extends Actor {
 
   import context._
       println("Job starting!")
-  for (a_task <- tasks) {
-    val task_ref: ActorRef = context.actorOf(Props(new Task(tasks(a_task), tasks)), a_task)
-    task_ref ! "init"
+  for (a_task <- tasks.get_tasks() ) {
+    val task_ref: ActorRef = context.actorOf(Props(new Task(a_task, tasks)), a_task)
+    task_ref ! "start"
   }
 
   
@@ -63,11 +130,12 @@ class Job(name: String, tasks: Map[String, Map[String,String]]) extends Actor {
   }
 }
 
-class Task(name: String, tasks: Map[String, Map[String,String]]) extends Actor {
+class Task(name: String, tg: StaticTaskGraph) extends Actor {
   import context._
 
   println("Task starting!")
   println(name)
+  tg.set_self(name)
   
 val cancellable =
   system.scheduler.schedule(
@@ -81,50 +149,26 @@ val cancellable =
       println("init")
       println(name)
     case "start" =>
-      println("start")
-//    Wait for a specific time
-//    Send start to downstream tasks
-
-    
+      val s = sender    // Actor[akka://<system>@<host>:<port>/user/path/to/actor]
+      val p = s.path    // akka://<system>@<host>:<port>/user/path/to/actor
+      val a = p.address // akka://<system>@<host>:<port>
+      val host = a.host // Some(<host>), where <host> is the listen address as configured for the remote system
+      val port = a.port
+      println(s"start $s $p $a $host $port")
+      println(a.toString.split("/"))
+      tg.task_complete("step1")
+      if (tg.ready()) {
+           println("task complete")
+      }
+      else {
+           println("task incomplete")
+      }
+      val details = tg.details()
+      println(s"executing task $details")
+      val next = tg.succ()
+      sender ! "start"
   }
 }
 
-/*
-val cache = collection.mutable.Map[String, collection.mutable.Map[String, String]]()
-val cache1 = collection.mutable.Map[String, String]()
-
-val map1 = Map("this"->Map("this1"->"xxx"),"that"->Map("this1"->"xxx"),"other"->Map("this1"->"xxx"))
-val map2 = collection.mutable.Map(map1.toSeq: _*) 
-println("test")
-
-//cache("test").
-cache1("this") = "abc"
-cache("test") = cache1
-println(cache("test"))
-cache("test")("this") = "xxx"
-println(map2)
 
 
-// This is a nice feature of Scala Maps:
-val defaultMap = Map("foo" -> 1, "bar" -> 2).withDefaultValue(3)
-defaultMap("qux")
-
-
-val task_status = scala.collection.mutable.Map[String, Int]()
-
-for each task in task_list:
-    task_status[task] = 0
-	
-wait for random time
-	
-rcv msg == "wake":
-    task_status[sender.name] += 1
-    upstream = getUpStream(name)
-    for each task in upstream_list:
-        if task_status(task) == 0:
-            return
-    for each task in downstream:
-        task ! "wake"
-	
-
-*/
