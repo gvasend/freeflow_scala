f97361d3035d2a41f460f6bd9beb7b96edb83d3e
Jerry Vasend <gvasend@gmail.com>
2018-01-11 13:09:12 -0500

    xx

--- scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
+++ scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
@@ -55,6 +55,7 @@ class StaticTaskGraph(tasks: Map[String, Map[String, Any]]) {
           { 
             if (x != "null") {
               println(s"send to $x")
+              implicit val timeout = Timeout(FiniteDuration(1, TimeUnit.SECONDS))
               ActorSystem("sentient_fabric").actorSelection("user/" + "somename").resolveOne().onComplete {
                 case Success(actorRef) => // logic with the actorRef
                 case Failure(ex) => Logger.warn("user/" + "somename" + " does not exist")}
