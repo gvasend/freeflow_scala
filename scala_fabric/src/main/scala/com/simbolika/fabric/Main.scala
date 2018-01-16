9181a0078169ad6e0841ce86d600d02fc2aa9bd9
Jerry Vasend <gvasend@gmail.com>
2018-01-16 16:42:13 -0500

    save service output

--- scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
+++ scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
@@ -304,7 +304,8 @@ class TaskInstance(tiid: Int, tg: NeoTaskGraph) extends Actor {
   var task_output: String = ""
   println(s"$tiid Task initializing")
 //  val istr = new ByteArrayInputStream("did this print?".getBytes("UTF-8"))
-  val out = ("cat" #< new File("/etc/passwd")).lineStream_!
+  val out = ("cat" #< new File("/etc/passwd")).!!
+  println("+++",out,"+++")
 
 
 val cancellable =
