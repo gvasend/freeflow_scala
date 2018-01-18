4a98b2d956f75ccbdea125efdbe0f155994ba948
Jerry Vasend <gvasend@gmail.com>
2018-01-18 16:13:04 -0500

    multi-task input

--- scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
+++ scala_fabric/src/main/scala/com/simbolika/fabric/Main.scala
@@ -329,7 +329,7 @@ val cancellable =
 	
   def executeProcess(cmd: String, inp: String): String = {
      var txt: String = ""
-//	 println(s"execute command: $cmd")
+	 println(s"execute command: $cmd")
      val calcProc = cmd.run(new ProcessIO(
       // Handle subprocess's stdin
       // (which we write via an OutputStream)
